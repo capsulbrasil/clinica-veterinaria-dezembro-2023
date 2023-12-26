8644252c4af5c1b7fec6434ca8d59ec659a2bea8
Anselmo <aspjunior.prof@gmail.com>
2023-12-26 13:11:17 -0300

    fixed description

---
+++ api/src/collections/geolocation/description.ts
@@ -0,0 +1,43 @@
+import { defineDescription } from "sonata-api";
+
+export const [Geolocation, description] = defineDescription({
+    $id: 'geolocation',
+    required: [
+        'country',
+        'state',
+        'district',
+        'street',
+        'number',
+        'zipcode'
+    ],
+    properties: {
+        country: {
+            description: 'País',
+            type: 'string',    
+        },
+        state: {
+            description: 'Estado',
+            type: 'string',
+        },
+        district: {
+            description: 'Bairro',
+            type: 'string'
+        },
+        street: {
+            description: 'Rua',
+            type: 'string',
+        },
+        number: {
+            description: 'Número',
+            type: 'number',
+        },
+        complement: {
+            description: 'Complemento',
+            type: 'string'
+        },
+        zipcode: {
+            description: 'Caixa Postal(CEP)',
+            type: 'string',
+        },
+    }
+})
